051cbaff6835673c76256125b7168cf7fdaae71b
romil chandragade <raxfake@gmail.com>
2017-02-19 20:29:04 +0530

    Add new property IsSubscribedToNewsLetter in Customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,5 +9,6 @@ namespace WebApplication1.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
     }
 }
